376ee658e9753321952b8e3aade40e28be669299
Ashwini277 <devarushiashwini@gmail.com>
2024-07-31 23:20:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-//your JS code here. If required.
+describe('Compact Gallery Tests', () => {
+    it('tests gallery heading and structure', () => {
+        cy.get('h2').should('have.text', 'Cards Gallery');
+        cy.get('.container .row').should('have.length', 1);
+    });
+
+    it('tests tags within cards', () => {
+        const tags = ['img', 'h6', 'p'];
+        tags.forEach(tag => {
+            cy.get(tag).should('have.length', 6);
+        });
+    });
+
+    it('tests card classes', () => {
+        cy.get('.card').should('have.length.at.least', 6).within(() => {
+            cy.get('.card-body').should('exist');
+        });
+        cy.get('a[data-featherlight="image"]').should('have.length.at.least', 6);
+    });
+
+    it('tests styling', () => {
+        const tags = ['h2', 'h6', 'p'];
+        tags.forEach(tag => cy.get(tag).should('have.css', 'text-align', 'center'));
+        cy.get('.card').should('have.css', 'border-radius', '10px');
+    });
+});
+
